7e2ec0a1afac73dcb06f93a1fb7d1eb7ce7fd1a4
ihorshto <igorshtogrin2016@gmail.com>
2022-06-13 14:24:53 +0300

    22 lesson route exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,24 @@
 import s from './Dialogs.module.css';
 
 const Dialogs = () => {
- return (
-  <div>
-    Dialogs
-  </div>
- )
+  return (
+    <div>
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+          <div className={s.dialog + ' ' + s.active}>Ihor</div>
+          <div className={s.dialog}>Tolik</div>
+          <div className={s.dialog}>Zenuk</div>
+          <div className={s.dialog}>Masha</div>
+          <div className={s.dialog}>Olga</div>
+        </div>
+        <div className={s.messages}>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>Yo IT komasutra</div>
+          <div className={s.message}>Nice stuff</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Dialogs;
