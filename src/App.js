875fa5b7a8dc74dc1525f5bfa0ac6c74d5d09067
ihorshto <igorshtogrin2016@gmail.com>
2022-06-13 13:30:06 +0300

    20 lesson - NavLink

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 
+
 function App() {
   return (
-    
+      <BrowserRouter>
         <div className='app-wrapper'>
           <Header/>
           <Navbar/>
           <div className='app-wrapper-content'>
-            <Router>
               <Routes>
-                <Route path='/profile/*' element={<Profile/>} />
-                <Route path='/dialogs/*' element={<Dialogs/>} />
+                <Route path='/profile' element={<Profile/>} />
+                <Route path='/dialogs' element={<Dialogs/>} />
+                <Route path='/news' element={<News/>} />
+                <Route path='/music' element={<Music/>} />
+                <Route path='/settings' element={<Settings/>} />
               </Routes>
-            </Router>
           </div>
         </div>
+      </BrowserRouter>
   );  
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <ul className={s.nav__list}>
-        <li className={`${s.item} ${s.active}`}>
-          <a href="/profile">Profile</a>
+        <li className={s.item}>
+          <NavLink to='/profile' className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink>
         </li>
         <li className={s.item}>
-          <a href='/dialogs' >Messages</a>
+          <NavLink to='/dialogs' className={navData => navData.isActive ? s.active : s.item}>Messages</NavLink>
         </li>
         <li className={s.item}>
-          <a href='/news' >News</a>
+          <NavLink to='/news' className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
         </li>
         <li className={s.item}>
-          <a href='music' >Music</a>
+          <NavLink to='music' className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
         </li>
         <li className={s.item}>
-          <a href='settings' >Settings</a>
+          <NavLink to='settings' className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
         </li>
       </ul>
     </nav>
