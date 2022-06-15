68017a5596c1b1fe6d1694703211e034280691d2
ihorshto <igorshtogrin2016@gmail.com>
2022-06-15 19:51:21 +0300

    31 lesson onClick,ref, VirtualDOM

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
@@ -8,6 +9,13 @@ const Dialogs = (props) => {
   let dialogsElements = props.state.dialogs.map ( d => <DialogItem name={d.name} id={d.id} />);
   let messagesElements = props.state.messages.map( m => <Message message={m.message} />)
 
+  let newMessage = React.createRef();
+
+  let AddMessage = () => {
+    let message = newMessage.current.value;
+    alert(message);
+  }
+
   return (
     <div>
       <div className={s.dialogs}>
@@ -16,6 +24,12 @@ const Dialogs = (props) => {
         </div>  
         <div className={s.messages}>
           { messagesElements }
+          <div>
+            <textarea ref={newMessage}></textarea>
+          </div>
+          <div>
+            <button onClick={AddMessage}>Add Message</button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,28 @@
+import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-
-
  let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} /> );
+
+ let newPostElement = React.createRef();
+
+ let addPost = () => {
+  let text = newPostElement.current.value;
+  alert(text);
+ }
+
+
 
  return (
   <div className={s.postsBlock}>
-   My posts
+   <h3>My posts</h3>
    <div>
     <div>
-     <textarea cols="30" rows="5"></textarea>
+     <textarea cols="30" rows="5" ref={newPostElement}></textarea>
     </div>
     <div>
-     <button>Add post</button>
+     <button onClick={addPost}>Add post</button>
     </div>
    </div>
    <div className={s.posts}>
