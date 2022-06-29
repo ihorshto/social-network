473cdf3af5195aeec99a2fd9cc98e753d086615c
ihorshto <igorshtogrin2016@gmail.com>
2022-06-29 21:59:27 +0300

    75 lesson form(formik) login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,51 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import loginFormSchema from "../FormValidation/LoginFormSchema";
 
-const Login = (props) => {
- return  <div>
-  <h1>LOGIN</h1>
- </div>
-}
+
+const LoginForm = () => {
+return	<div>
+		<Form>
+			<div>
+				<Field type={'text'} name={'email'} placeholder={'e-mail'} />
+			</div>
+			<ErrorMessage name="email" component="div" />
+			<div>
+				<Field type={'password'} name={'password'} placeholder={'password'} />
+			</div>
+			<ErrorMessage name="password" component="div" />
+			<div>
+				<Field type={'checkbox'} name={'rememberMe'} />
+				<label htmlFor={'rememberMe'}> remember me </label>
+			</div>
+			<button type={'submit'}>Log in</button>
+		</Form>
+	</div>
+};
+
+const Login = () => {
+	return (
+	<div>
+		<h1>Login</h1>
+		<Formik
+			initialValues={{ email: "", password: "", rememberMe: false }}
+			validate={values => {
+				const errors = {};
+				if (!values.email) {
+					errors.email = 'Required';
+				}
+				return errors;
+			}}
+			onSubmit={(values) => {
+				console.log(values)
+			}}
+			validationSchema={loginFormSchema}>
+			{() => (
+				<LoginForm/>
+			)}
+		</Formik>
+	</div>
+)};
+
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -11,7 +11,7 @@ let reducers = combineReducers({
 	dialogsPage: dialogsReducer,
 	sidebar: sidebarReducer,
 	usersPage: usersReducer,
-	auth: authReducer
+	auth: authReducer,
 }); 
 
 
