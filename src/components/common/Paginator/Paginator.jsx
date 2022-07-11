a4172d23d7860405bbf3064f650c50e49b38c515
ihorshto <igorshtogrin2016@gmail.com>
2022-07-11 15:37:02 +0300

    93 lesson - pagination

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Paginator.module.css';
+import cn from 'classnames';
 
-let Paginator = (props) => {
-  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
+
   let pages = [];
-
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
+
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumber] = useState(1);
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+
   return (
-    <div>
-        {pages.map(p => {
-          return <span className={props.currentPage === p ? s.selectedPage : ""}
-            onClick={(e) => {props.onPageChanged(p); }} >{p}</span>
-        }
-        )}
+    <div className={s.paginator}>
+      {portionNumber > 1 &&
+      <button className={s.arrow_left} onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button>}
+
+        {pages
+        .filter(p => p>= leftPortionPageNumber && p <= rightPortionPageNumber)
+        .map((p) => {
+          return <span className={ cn(
+            {[s.selectedPage] : currentPage === p},
+             s.pageNumber)}
+                key={p} onClick={(e) => {onPageChanged(p);
+          }} >{p}</span>
+        })}
+      {portionCount > portionNumber &&
+        <button className={s.arrow_right} onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button>}
     </div>
   )
 }
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -13,7 +13,7 @@ const TOOGLE_IS_FOLLOWING_PROGRESS = 'TOOGLE_IS_FOLLOWING_PROGRESS';
 
 let initialState = {
 	users: [],
-	pageSize: 5,
+	pageSize: 10,
 	totalUsersCount: 0,
 	currentPage: 1,
 	isFetching: false,
