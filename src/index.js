5c20477aac9617ba8cfa1e632e28a5c41122c3b1
ihorshto <igorshtogrin2016@gmail.com>
2022-07-12 09:54:14 +0300

    94 lesson -  react.Suspense react lazy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
   root.render(
     <React.StrictMode>
-      <MainApp id="app" />
+      <MainApp/>
     </React.StrictMode>
   ); 
 
